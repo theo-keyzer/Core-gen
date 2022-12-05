10a295e9f22f007a989e9e36d77e51b4be9fda79
Theo Keyzer <theo.keyzer@gmail.com>
2022-12-05 13:26:01 +0200

    Refine match actors

--- Crystal/info/run.sh
+++ Crystal/info/run.sh
@@ -1,1 +1,15 @@
+echo note.act
 ./gen note.act note.def
+echo m.act
+./gen m.act note.def info name
+echo m2.act
+./gen m2.act note.def info name
+echo m3.act
+./gen m3.act note.def info name
+echo m4.act
+./gen m4.act note.def info model
+echo m5.act
+./gen m5.act note.def pos todo
+echo m6.act
+./gen m6.act note.def todo pos
+
