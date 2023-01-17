bd9dfc22e079fd87c51f86ed4e31ce47fa69a291
Theo Keyzer <theo.keyzer@gmail.com>
2023-01-17 13:27:45 +0200

    Added sample units

--- Dart/examples/gen.sh
+++ Dart/examples/gen.sh
@@ -1,7 +1,7 @@
-~/crystal/test3/gen d_struct.act gen.unit,act.unit >gen1/bin/structs.dart
+~/crystal/test3/gen d_struct.act gen.unit,act.unit,app.unit,sample.unit,note.unit >gen1/bin/structs.dart
 if [ $? != 0 ]; then echo c_struct.act has errors; fi
 
-~/crystal/test3/gen d_run.act gen.unit,act.unit >gen1/bin/run.dart
+~/crystal/test3/gen d_run.act gen.unit,act.unit,app.unit,sample.unit,note.unit >gen1/bin/run.dart
 if [ $? != 0 ]; then echo c_run.act has errors; fi
 
 
--- Dart/examples/run.sh
+++ Dart/examples/run.sh
@@ -1,5 +1,5 @@
-dart run gen1/bin/gen.dart d_run.act gen.unit,act.unit >rr
-dart run gen1/bin/gen.dart d_struct.act gen.unit,act.unit >ss
+dart run gen1/bin/gen.dart d_run.act gen.unit,act.unit,app.unit,sample.unit,note.unit >rr
+dart run gen1/bin/gen.dart d_struct.act gen.unit,act.unit,app.unit,sample.unit,note.unit >ss
 
 diff ss gen1/bin/structs.dart
 
