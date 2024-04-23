5ba8ff395cf3e5ab181924b4dd79a4c5afbe226a
Theo Keyzer <theo.keyzer@gmail.com>
2024-04-23 19:11:32 +0200

    Added get_list for stand alone apps

--- python/bld/gen.sh
+++ python/bld/gen.sh
@@ -1,6 +1,9 @@
 python ../src/main.py p_struct.act gen.unit,act.unit >src/structs.py
 if [ $? != 0 ]; then echo p_struct.act gen.unit,act.unit has errors; fi
 
+python ../src/main.py p_struct2.act gen.unit,act.unit >src/structs2.py
+if [ $? != 0 ]; then echo p_struct2.act gen.unit,act.unit has errors; fi
+
 python ../src/main.py p_run.act gen.unit,act.unit >src/run.py
 if [ $? != 0 ]; then echo p_run.act gen.unit,act.unit has errors; fi
 
