47add45c35d515933c3172337511d86b7347bebc
Theo Keyzer <theo.keyzer@gmail.com>
2024-05-09 15:01:50 +0200

    Added dag test

--- python/app/tst/test.sh
+++ python/app/tst/test.sh
@@ -1,8 +1,10 @@
-python ../src/main.py tst2.act tst.def,sample.def a b c d
+python ../src/main.py mtst.act note.def
 
-python ../src/main.py match1.act note.def thinking,frame
+python ../src/main.py match1.act note.def thinking,frame,group,join,layout
 
 python ../src/main.py daoimpl.act sample.def >user.java
 python ../src/main.py view.act sample.def >user.sql
 
+python ../src/main.py  dag.act tst.def
+python ../src/main.py tst2.act tst.def,sample.def a b c d
 
