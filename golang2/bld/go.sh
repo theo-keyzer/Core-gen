a66501604621e1a0dc4abfb358ff376354aa20a5
Theo Keyzer <theo.keyzer@gmail.com>
2025-01-09 16:56:03 +0200

    Testing

---
+++ golang2/bld/go.sh
@@ -0,0 +1,3 @@
+~/go/go1.23.4.linux-amd64/go/bin/go run ../gen/main.go ../gen/gen.go ../gen/run.go ../gen/structs.go g_run.act gen.unit,act.unit 
+~/go/go1.23.4.linux-amd64/go/bin/go run ../gen/main.go ../gen/gen.go ../gen/run.go ../gen/structs.go g_struct.act gen.unit,act.unit
+
